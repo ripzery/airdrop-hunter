ff8e4ed5c214f9389cbf5bf96951c6678df5a56c
euro <ripzery@gmail.com>
2021-02-04 15:47:27 +0700

    add deploy scripts for truffle

--- src/contracts/contracts/EtherMultiSender.sol
+++ src/contracts/contracts/EtherMultiSender.sol
@@ -1,18 +1,19 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.7.0;
 import './SafeMath.sol';
 
 
-contract ERC20Basic {
-    function totalSupply() public view returns (uint256);
-    function balanceOf(address who) public view returns (uint256);
-    function transfer(address to, uint256 value) public returns (bool);
+abstract contract ERC20Basic {
+    function totalSupply() virtual public view returns (uint256);
+    function balanceOf(address who) virtual public view returns (uint256);
+    function transfer(address to, uint256 value) virtual public returns (bool);
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract ERC20 is ERC20Basic {
-    function allowance(address owner, address spender) public view returns (uint256);
-    function transferFrom(address from, address to, uint256 value) public returns (bool);
-    function approve(address spender, uint256 value) public returns (bool);
+abstract contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) virtual public view returns (uint256);
+    function transferFrom(address from, address to, uint256 value) virtual public returns (bool);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
@@ -31,12 +32,12 @@ contract EtherMultiSender {
         }
     }
 
-    function multisendErc20(address token, address[] _recipients, uint256[] _balances) public payable {
+    function multisendErc20(address token, address payable[] memory _recipients, uint256[] memory _balances) public payable {
         require(_recipients.length <= 200);
 
         uint total = 0;
         uint256 i = 0;
-        Erc20 erc20 = Erc20(token);
+        ERC20 erc20 = ERC20(token);
 
         for(i; i < _recipients.length; i++) {
             require(total >= _balances[i]);
--- src/contracts/truffle-config.js
+++ src/contracts/truffle-config.js
@@ -1,4 +1,38 @@
+require('dotenv').config();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard BSC port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+    testnet: {
+      provider: () => new HDWalletProvider(process.env.SENDER_PRIVATE_KEY, process.env.TESTNET_PROVIDER),
+      network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    bsc: {
+      provider: () => new HDWalletProvider(process.env.SENDER_PRIVATE_KEY, process.env.PROVIDER),
+      network_id: 56,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.7.0"
+    }
+  }
+}
